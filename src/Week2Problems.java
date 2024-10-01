d00fb0c24a5f8940e915f698126619f2c7128317
bhavishjagani <bhavishjagani2@gmail.com>
2024-09-30 21:45:12 -0700

    Week 2 Problems

--- src/Week2Problems.java
+++ src/Week2Problems.java
@@ -10,7 +10,7 @@ public class Week2Problems {
     public static void main (String[] args) {
         int[] searchNums1 = {4, 5, 6, 7, 0, 1, 2};
         int[] searchNums2 = {1};
-        System.out.println("Problem 1: \n");
+        System.out.println("Problem 1:  \n");
         System.out.println(search(searchNums1, 0));
         System.out.println(search(searchNums1, 3));
         System.out.println(search(searchNums2, 0));
