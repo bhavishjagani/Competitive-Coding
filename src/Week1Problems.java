51289acf1dd4512d4227ba974a56417a34584a4f
bhavishjagani <bhavishjagani2@gmail.com>
2024-09-30 21:40:33 -0700

    Week 2 Problems

--- src/Week1Problems.java
+++ src/Week1Problems.java
@@ -46,50 +46,50 @@ public class Week1Problems {
         return nums[0];
     }
     public static void main (String[] args) {
-//        int[] twoSumNums1 = {2,7,11,15};
-//        int[] twoSumNums2 = {3,2,4};
-//        int[] twoSumNums3 = {3,3};
-//        System.out.println("Problem 1: \n");
-//        System.out.println(Arrays.toString(twoSum(twoSumNums1, 9)));
-//        System.out.println(Arrays.toString(twoSum(twoSumNums2, 6)));
-//        System.out.println(Arrays.toString(twoSum(twoSumNums3, 6)));
-//        System.out.println();
-//
-//        int[] prices1 = {7,1,5,3,6,4};
-//        int[] prices2 = {7,6,4,3,1};
-//        System.out.println("Problem 2: \n");
-//        System.out.println(maxProfit(prices1));
-//        System.out.println(maxProfit(prices2));
-//        System.out.println();
-//
-//        int[] containsDuplicateNums1 = {1,2,3,1};
-//        int[] containsDuplicateNums2 = {1,2,3,4};
-//        int[] containsDuplicateNums3 = {1,1,1,3,3,4,3,2,4,2};
-//        System.out.println("Problem 3: \n");
-//        System.out.println(containsDuplicate(containsDuplicateNums1));
-//        System.out.println(containsDuplicate(containsDuplicateNums2));
-//        System.out.println(containsDuplicate(containsDuplicateNums3));
-//        System.out.println();
-//
-//        System.out.println("Problem 4: \n");
-//
-//        int[] maxSubArrayNums1 = {-2,1,-3,4,-1,2,1,-5,4};
-//        int[] maxSubArrayNums2 = {1};
-//        int[] maxSubArrayNums3 = {5, 4, -1, 7, 8};
-//        System.out.println("Problem 5: \n");
-//        System.out.println(maxSubArray(maxSubArrayNums1));
-//        System.out.println(maxSubArray(maxSubArrayNums2));
-//        System.out.println(maxSubArray(maxSubArrayNums3));
-//        System.out.println();
-//
-//        System.out.println("Problem 6: \n");
-//
-//        int[] findMinNums1 = {3, 4, 5, 1, 2};
-//        int[] findMinNums2 = {4, 5, 6, 7, 0, 1, 2};
-//        int[] findMinNums3 = {11, 13, 15, 17};
-//        System.out.println("Problem 7: ");
-//        System.out.println(findMin(findMinNums1));
-//        System.out.println(findMin(findMinNums2));
-//        System.out.println(findMin(findMinNums3));
+        int[] twoSumNums1 = {2,7,11,15};
+        int[] twoSumNums2 = {3,2,4};
+        int[] twoSumNums3 = {3,3};
+        System.out.println("Problem 1: \n");
+        System.out.println(Arrays.toString(twoSum(twoSumNums1, 9)));
+        System.out.println(Arrays.toString(twoSum(twoSumNums2, 6)));
+        System.out.println(Arrays.toString(twoSum(twoSumNums3, 6)));
+        System.out.println();
+
+        int[] prices1 = {7,1,5,3,6,4};
+        int[] prices2 = {7,6,4,3,1};
+        System.out.println("Problem 2: \n");
+        System.out.println(maxProfit(prices1));
+        System.out.println(maxProfit(prices2));
+        System.out.println();
+
+        int[] containsDuplicateNums1 = {1,2,3,1};
+        int[] containsDuplicateNums2 = {1,2,3,4};
+        int[] containsDuplicateNums3 = {1,1,1,3,3,4,3,2,4,2};
+        System.out.println("Problem 3: \n");
+        System.out.println(containsDuplicate(containsDuplicateNums1));
+        System.out.println(containsDuplicate(containsDuplicateNums2));
+        System.out.println(containsDuplicate(containsDuplicateNums3));
+        System.out.println();
+
+        System.out.println("Problem 4: \n");
+
+        int[] maxSubArrayNums1 = {-2,1,-3,4,-1,2,1,-5,4};
+        int[] maxSubArrayNums2 = {1};
+        int[] maxSubArrayNums3 = {5, 4, -1, 7, 8};
+        System.out.println("Problem 5: \n");
+        System.out.println(maxSubArray(maxSubArrayNums1));
+        System.out.println(maxSubArray(maxSubArrayNums2));
+        System.out.println(maxSubArray(maxSubArrayNums3));
+        System.out.println();
+
+        System.out.println("Problem 6: \n");
+
+        int[] findMinNums1 = {3, 4, 5, 1, 2};
+        int[] findMinNums2 = {4, 5, 6, 7, 0, 1, 2};
+        int[] findMinNums3 = {11, 13, 15, 17};
+        System.out.println("Problem 7: ");
+        System.out.println(findMin(findMinNums1));
+        System.out.println(findMin(findMinNums2));
+        System.out.println(findMin(findMinNums3));
     }
 }
